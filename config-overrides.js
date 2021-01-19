716d895acebeac3d39214b653779b7c8a07515bb
CrazySean007 <shuaim@usc.edu>
2021-01-19 11:27:48 -0800

    modify build files2

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,38 @@
 //implement pack files based on demands
 //pack based on all imports, use babel-plugin-import
-const {override, fixBabelImports, addLessLoader} = require('customize-cra');
-module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true, //automatically pack related css
-    }),
-    addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#1DA57A' },
-    }),
-);
+const {override, overrideDevServer, fixBabelImports, addLessLoader} = require('customize-cra');
+
+// 跨域配置
+const devServerConfig = () => config => {
+    return {
+        ...config,
+        // 服务开启gzip
+        compress: true,
+        proxy: {
+            '/api': {
+                target: 'xxx',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '/api',
+                },
+            }
+        }
+    }
+};
+
+module.exports = {
+    webpack: override(
+        fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true, //automatically pack related css
+        }),
+        addLessLoader({
+            javascriptEnabled: true,
+            modifyVars: {'@primary-color': '#1DA57A'},
+        }),
+    ),
+    devServer: overrideDevServer(
+        devServerConfig()
+    )
+};
